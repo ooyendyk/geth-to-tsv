f50f0b1f3078bc3325b26daa5f7f6f6350a37cda
Nerd-1 <ooyendyk@tuta.io>
2021-07-16 23:15:48 +0930

    Add loging functionality

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This app extracts data from a Geth node, and saves it in a TSV file
 
 Project Brief: https://docs.google.com/document/d/1dFDgdRc1EAIV4rWh3PYWCVCk0SVOlkjkz2W_E26UKP0/edit
+
+Start Clef:
+`clef --keystore <GETH_DATA_DIR>/keystore --chainid 5`
+
+Start Clef on my system: `clef --keystore ~/.ethereum/keystore --chainid 5`
+
+Start Geth: `geth --goerli --syncmode "light" --http --ws --signer=<CLEF_LOCATION>/clef.ipc`
+
+Start Geth on my system: `geth --goerli --syncmode "light" --http --ws --signer=/home/user/.clef/clef.ipc`
--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 // const Web3 = require('web3');
 // const converter = require('json-2-csv');
+// const ObjectsToCsv = require('objects-to-csv');
 const WebSocket = require('ws');
+const fs = require('fs');
 
-console.log("Running block extractor");
+// Clear log.tsv and add header
+console.log("Running ETL pipeline");
+const header = 'parentHash' + '\t' + 'sha3Uncles' + '\t' + 'miner' + '\t' + 'stateRoot' + '\t' + 'transactionsRoot' + '\t' + 'receiptsRoot' + '\t' + 'logsBloom' + '\t' + 'difficulty' + '\t' + 'number' + '\t' + 'gasLimit' + '\t' + 'gasUsed' + '\t' + 'timestamp' + '\t'+'extraData' + '\t' + 'mixHash' + '\t' + 'nonce' + '\t' + 'baseFeePerGas' + '\t' + 'hash';
+fs.writeFile('log.tsv', header, function(){console.log('Resetting log')});
 
+// Connect to Geth node websocket
 const ws = new WebSocket(`ws://127.0.0.1:8546`);
 ws.on('open', function open() {
     ws.send('{"jsonrpc":"2.0","method":"eth_subscribe","params":["newHeads"], "id":1}');
 });
 
+// Log each event
 ws.on('message', function incoming(data) {
     var obj = JSON.parse(data);
-    console.log(obj);
+    try {
+        var entry = '\n' + obj.params.result.parentHash + '\t' + obj.params.result.sha3Uncles + '\t' + obj.params.result.miner + '\t' + obj.params.result.stateRoot + '\t' + obj.params.result.transactionsRoot + '\t' + obj.params.result.receiptsRoot + '\t' + obj.params.result.logsBloom + '\t' + obj.params.result.difficulty + '\t' + obj.params.result.number + '\t' + obj.params.result.gasLimit + '\t' + obj.params.result.gasUsed + '\t' + obj.params.result.timestamp + '\t' + obj.params.result.extraData + '\t' + obj.params.result.mixHash + '\t' + obj.params.result.nonce + '\t' + obj.params.result.baseFeePerGas + '\t' + obj.params.result.hash;
+        fs.appendFileSync('log.tsv', entry);
+    } catch (e) {
+        console.log('ERR: obj.params.* not found ')
+    }
     // ws.close()
 });
